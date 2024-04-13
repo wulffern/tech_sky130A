8ba4354544308b3016cc914b0a4b16f44fa3f202
Carsten Wulff <carsten@wulff.no>
2024-04-13 22:05:02 +0200

    Added LVS after LPE. RC extraction not working for coupling caps

--- magic/lpe.tcl
+++ magic/lpe.tcl
@@ -1,13 +1,13 @@
 #set VDD AVDD
 #set GND AVSS
-set SUB 0
+#set SUB 0
 
 set OPATH lpe/ext
 
 load {PATH}/{CELL}.mag
 
-#flatten {CELL}_flat
-#load {CELL}_flat
+flatten {CELL}_flat
+load {CELL}_flat
 
 select top cell
 extract path ${OPATH}
@@ -17,6 +17,6 @@ extract all
 ext2spice lvs
 #ext2spice format ngspice
 #ext2spice resistor off
-ext2spice cthresh 0.05
+ext2spice cthresh 0.01
 ext2spice -p ${OPATH} -o lpe/{CELL}_lpe.spi
 quit
